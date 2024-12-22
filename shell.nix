ea9e288d570920d7d22a8205144ac663c20c8bad
Luis-Licea <Luis-Licea@users.noreply.github.com>
2024-12-22 20:40:07 +0000

    jekyll build from Action 1f3c87862edbcba726c0d9c45e77ca7641b3ef48

---
+++ shell.nix
@@ -0,0 +1,13 @@
+with import <nixpkgs> {}; let
+  # Use `nix-shell -p bundix --run "bundix -l"` to generate gemset.nix.
+  gems = bundlerEnv {
+    name = "gems";
+    gemdir = ./.;
+  };
+in
+  mkShell {
+    packages = [gems gems.wrappedRuby ruby];
+    shellHook = ''
+      source bashrc
+    '';
+  }
